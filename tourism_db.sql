71fbc76b3652650f7badf05e2a83be0411ce5b48
Neeharika Anand <131549925+neeharika-anand@users.noreply.github.com>
2023-11-23 10:15:28 +0530

    Update tourism_db.sql

--- tourism_db.sql
+++ tourism_db.sql
@@ -7,9 +7,10 @@ SHOW DATABASES;
 DROP DATABASE tourism;
 
 CREATE DATABASE tourism;
+
 -- Use the 'tourism' database
 USE tourism;
-DROP TABLE tour;
+DROP TABLE booking;
 select * from guests;
 -- Create tables
 CREATE TABLE customer (
@@ -25,18 +26,15 @@ CREATE TABLE customer (
     CONSTRAINT PRIMARY KEY (mail_id)
 );
 
-CREATE TABLE membership (
-    membership_id INT AUTO_INCREMENT,
-    type VARCHAR(15),
-    Offers VARCHAR(50),
-    CONSTRAINT PRIMARY KEY (membership_id)
-);
 
 CREATE TABLE guests (
     guest_fname VARCHAR(30),
     guest_lname VARCHAR(30),
     Age INT,
-    mail_id VARCHAR(30)
+    mail_id VARCHAR(30),
+    mobilenumber VARCHAR(30),
+    booking_id INT
+    
 );
 desc guests;
 
@@ -66,6 +64,7 @@ CREATE TABLE guide (
 CREATE TABLE booking (
     booking_id INT AUTO_INCREMENT,
     booking_date DATE,
+    no_of_guests INT,
     mail_id VARCHAR(30),
     package_id INT,
     tour_id INT,
@@ -89,31 +88,19 @@ CREATE TABLE tour (
     package_id INT,
     CONSTRAINT PRIMARY KEY (tour_id)
 );
-
+Drop table transport_agency;
 CREATE TABLE transport_agency (
-    vehicle_no INT,
-    vehicle_type VARCHAR(25),
+    vehicle_no VARCHAR(100),
+    vehicle_type enum('Mini Van', 'Traveller', 'Bus'),
     driver_ssn VARCHAR(25),
     driver_name VARCHAR(30),
     driver_num VARCHAR(20),
-    place_id VARCHAR(25),
+    tour_id INT,
     CONSTRAINT PRIMARY KEY (vehicle_no)
 );
 
 
-CREATE TABLE hotel (
-    hotel_id INT,
-    hotel_name VARCHAR(30),
-    address VARCHAR(30),
-    phone_no VARCHAR(20),
-    CONSTRAINT PRIMARY KEY (hotel_id)
-);
 
-
-CREATE TABLE package_hotel (
-    package_id INT,
-    hotel_id INT
-);
 
 
 
@@ -132,9 +119,6 @@ ALTER TABLE package_hotel ADD CONSTRAINT FOREIGN KEY (package_id) REFERENCES pac
 ALTER TABLE package_hotel ADD CONSTRAINT FOREIGN KEY (hotel_id) REFERENCES hotel (hotel_id) ON DELETE CASCADE ON UPDATE CASCADE;
 
 
-INSERT INTO guide (guide_fname, guide_lname, phone_no, package_id, mail_id, password) VALUES ('Bubs','Nee', '1234567890', '3', 'bubs@bubs.com', 'nee');
-
-
 
 -- Insert data into the 'package' table
 INSERT INTO package (package_id, price, place)
@@ -146,33 +130,234 @@ VALUES
     (5, 25000, 'Ooty & Coonoor'),
     (6, 25000, 'Bangalore & Mysore');
 
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (1, 'Claudie', 'Clementi', '3753105969', 3, 'cclementi0@illinois.edu', 'iD6k808');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (2, 'Raye', 'Meneur', '5252657292', 4, 'rmeneur1@dmoz.org', 'tH2lCjk(');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (3, 'Spense', 'Frail', '7514682262', 3, 'sfrail2@desdev.cn', 'yJ4Pfqu');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (4, 'Andrei', 'Keel', '2522409465', 4, 'akeel3@tiny.cc', 'jR22z.,+');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (5, 'Jud', 'O''Gleasane', '6505589366', 2, 'jogleasane4@last.fm', 'pB0grvcup');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (6, 'Kyrstin', 'Shortt', '8224517358', 3, 'kshortt5@techcrunch.com', 'aB63FG|');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (7, 'Reeta', 'Priestnall', '1177962957', 2, 'rpriestnall6@simplemachines.org', 'zA24}Z&@');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (8, 'Renelle', 'Megson', '4011657249', 5, 'rmegson7@google.es', 'jL1T38eU3');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (9, 'Marilyn', 'Corden', '6675841543', 5, 'mcorden8@over-blog.com', 'dV5o@xG)t>');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (10, 'Helene', 'O''Noulane', '1407407137', 2, 'honoulane9@slate.com', 'yV9v=cpr{');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (11, 'Cally', 'Croux', '6928741472', 3, 'ccrouxa@mlb.com', 'cL3UNJ0.');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (12, 'Anna-maria', 'Blackden', '6862817718', 4, 'ablackdenb@virginia.edu', 'eP95L%nMZ');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (13, 'Cristionna', 'Bowstead', '4088702879', 5, 'cbowsteadc@ted.com', 'kU2y"');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (14, 'Egor', 'McCaughey', '4782611215', 5, 'emccaugheyd@feedburner.com', 'lJ1\sTM{');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (15, 'Jacqui', 'Klamp', '8651553518', 5, 'jklampe@icio.us', 'yU3#bR');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (16, 'Rica', 'Tomasini', '5083198601', 3, 'rtomasinif@shop-pro.jp', 'tF8i''X)!w');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (17, 'Daryl', 'Lichfield', '7953282837', 1, 'dlichfieldg@squidoo.com', 'jY11?');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (18, 'Chilton', 'Qualtrough', '2769503488', 1, 'cqualtroughh@indiatimes.com', 'xL1FHH_a');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (19, 'Shandeigh', 'Skeermor', '2741138345', 5, 'sskeermori@last.fm', 'bJ2M)o"');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (20, 'Janeva', 'Condon', '5926778122', 6, 'jcondonj@google.de', 'hS7u5<3|M"');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (21, 'Othelia', 'Wybourne', '2948384385', 1, 'owybournek@ox.ac.uk', 'hF7v1,3,');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (22, 'Casi', 'Cheak', '3723004392', 1, 'ccheakl@ameblo.jp', 'pD2_uhdA');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (23, 'Saundra', 'Blaker', '3884304627', 3, 'sblakerm@upenn.edu', 'iL0H%VJZ#');
-insert into guide (guide_id, guide_fname, guide_lname, phone_no, package_id, mail_id, password) values (24, 'Sanders', 'Lippo', '7672420662', 5, 'slippon@engadget.com', 'zE4YU*q~3');
+-- Inserts for package_id = 1
+INSERT INTO guide (guide_fname, guide_lname, phone_no, package_id, mail_id, password) VALUES
+('John', 'Doe', '1234567890', 1, 'john.doe@example.com', 'password123'),
+('Alice', 'Smith', '9876543210', 1, 'alice.smith@example.com', 'password456');
+
+-- Inserts for package_id = 2
+INSERT INTO guide (guide_fname, guide_lname, phone_no, package_id, mail_id, password) VALUES
+('Bob', 'Johnson', '1112223333', 2, 'bob.johnson@example.com', 'password789'),
+('Eva', 'Williams', '4445556666', 2, 'eva.williams@example.com', 'passwordabc');
+
+-- Inserts for package_id = 3
+INSERT INTO guide (guide_fname, guide_lname, phone_no, package_id, mail_id, password) VALUES
+('Charlie', 'Miller', '7778889999', 3, 'charlie.miller@example.com', 'passwordxyz'),
+('Grace', 'Taylor', '3334445555', 3, 'grace.taylor@example.com', 'password123');
+
+-- Inserts for package_id = 4
+INSERT INTO guide (guide_fname, guide_lname, phone_no, package_id, mail_id, password) VALUES
+('Daniel', 'Brown', '5556667777', 4, 'daniel.brown@example.com', 'password456'),
+('Olivia', 'Davis', '8889990000', 4, 'olivia.davis@example.com', 'password789');
+
+-- Inserts for package_id = 5
+INSERT INTO guide (guide_fname, guide_lname, phone_no, package_id, mail_id, password) VALUES
+('Edward', 'Hall', '2223334444', 5, 'edward.hall@example.com', 'passwordabc'),
+('Sophia', 'Young', '9990001111', 5, 'sophia.young@example.com', 'passwordxyz');
+
+-- Inserts for package_id = 6
+INSERT INTO guide (guide_fname, guide_lname, phone_no, package_id, mail_id, password) VALUES
+('Frank', 'Ward', '6667778888', 6, 'frank.ward@example.com', 'password123'),
+('Isabella', 'Adams', '1112223333', 6, 'isabella.adams@example.com', 'password456');
+
+
+INSERT INTO transport_agency VALUES ('KA01AB1234', 'Mini Van', '123-45-6789', 'John Doe', '9876543210', NULL);
+INSERT INTO transport_agency VALUES ('MH02CD5678', 'Traveller', '234-56-7890', 'Jane Smith', '8765432109', NULL);
+INSERT INTO transport_agency VALUES ('TN03EF9012', 'Bus', '345-67-8901', 'Robert Johnson', '7654321098', NULL);
+INSERT INTO transport_agency VALUES ('AP04GH2345', 'Mini Van', '456-78-9012', 'Emily Brown', '6543210987', NULL);
+INSERT INTO transport_agency VALUES ('KA05IJ3456', 'Traveller', '567-89-0123', 'Michael Davis', '5432109876', NULL);
+INSERT INTO transport_agency VALUES ('MH06KL4567', 'Bus', '678-90-1234', 'Sophia Wilson', '4321098765', NULL);
+INSERT INTO transport_agency VALUES ('TN07MN5678', 'Mini Van', '789-01-2345', 'Daniel Miller', '3210987654', NULL);
+INSERT INTO transport_agency VALUES ('AP08OP6789', 'Traveller', '890-12-3456', 'Olivia Taylor', '2109876543', NULL);
+INSERT INTO transport_agency VALUES ('KA09QR7890', 'Bus', '901-23-4567', 'Ethan Anderson', '1098765432', NULL);
+INSERT INTO transport_agency VALUES ('MH10ST8901', 'Mini Van', '012-34-5678', 'Ava Garcia', '9876543210', NULL);
+INSERT INTO transport_agency VALUES ('TN11UV9012', 'Traveller', '123-45-6789', 'Liam Smith', '8765432109', NULL);
+INSERT INTO transport_agency VALUES ('AP12WX1234', 'Bus', '234-56-7890', 'Emma Johnson', '7654321098', NULL);
+INSERT INTO transport_agency VALUES ('KA13YZ2345', 'Mini Van', '345-67-8901', 'Noah Brown', '6543210987', NULL);
+INSERT INTO transport_agency VALUES ('MH14AB3456', 'Traveller', '456-78-9012', 'Isabella Davis', '5432109876', NULL);
+INSERT INTO transport_agency VALUES ('TN15CD4567', 'Bus', '567-89-0123', 'Aiden Wilson', '4321098765', NULL);
+INSERT INTO transport_agency VALUES ('AP16EF5678', 'Mini Van', '678-90-1234', 'Mia Miller', '3210987654', NULL);
+INSERT INTO transport_agency VALUES ('KA17GH6789', 'Traveller', '789-01-2345', 'James Taylor', '2109876543', NULL);
+INSERT INTO transport_agency VALUES ('MH18IJ7890', 'Bus', '890-12-3456', 'Sophia Anderson', '1098765432', NULL);
+INSERT INTO transport_agency VALUES ('TN19KL8901', 'Mini Van', '901-23-4567', 'Logan Garcia', '9876543210', NULL);
+INSERT INTO transport_agency VALUES ('AP20MN9012', 'Traveller', '012-34-5678', 'Ava Smith', '8765432109', NULL);
 
 
 
-insert into tour (numofpeople, start_date, end_date, package_id, tour_status) values(0, '2023-11-16', '2023-11-20', 1, 'Upcoming');
-SELECT start_date from tour WHERE package_id = 1 AND tour_status = 'Upcoming';
-insert into tour (numofpeople, start_date, end_date, package_id, tour_status) values(0, '2023-09-10','2023-09-14', 1, 'Completed');
+DROP PROCEDURE CreateNextTourDate;
+
+DELIMITER //
+
+CREATE PROCEDURE CreateNextTourDate()
+BEGIN
+    DECLARE current_tour_id INT;
+    DECLARE current_package_id INT;
+    DECLARE new_tour_id INT;
+    DECLARE current_start_date DATE;
+    DECLARE new_start_date DATE;
+	DECLARE new_end_date DATE;
+    -- Fetch tour details for the upcoming tour that starts one day from now
+    SELECT tour_id, package_id, start_date INTO current_tour_id, current_package_id, current_start_date
+    FROM tour
+    WHERE tour_status = 'Upcoming' AND start_date = DATE_ADD(CURDATE(), INTERVAL 1 DAY);
+
+    -- Calculate the new end date for the tour (5 days from the new start date)
+    SET new_start_date = DATE_ADD(current_start_date, INTERVAL 2 WEEK);
+    SET new_end_date = DATE_ADD(new_start_date, INTERVAL 5 DAY);
+
+-- Insert a new tour for the same package 2 weeks after the current tour's start date
+INSERT INTO tour (numofpeople, start_date, end_date, tour_status, guide_id, package_id)
+VALUES (0, new_start_date, new_end_date, 'Upcoming', NULL, current_package_id);
+    -- Retrieve the last inserted ID, which will be the new tour's ID
+    SET new_tour_id = LAST_INSERT_ID();
+
+    -- Update the current tour to Ongoing status
+    UPDATE tour
+    SET tour_status = 'Ongoing'
+    WHERE tour_id = current_tour_id;
+
+END //
+
+DELIMITER ;
+DROP EVENT CheckTourDate;
+DELIMITER //
+
+
+DROP EVENT CheckTourDate;
+CREATE EVENT IF NOT EXISTS CheckTourDate
+ON SCHEDULE EVERY 1 MINUTE
+DO
+BEGIN
+    DECLARE current_tour_id INT;
+    DECLARE current_package_id INT;
+    DECLARE new_tour_id INT;
+    DECLARE new_start_date DATE;
+
+    SELECT tour_id, package_id, start_date INTO current_tour_id, current_package_id, new_start_date
+    FROM tour
+    WHERE tour_status = 'Upcoming' AND start_date = DATE_ADD(CURDATE(), INTERVAL 1 DAY);
+
+    IF current_tour_id IS NOT NULL THEN
+        CALL CreateNextTourDate(); -- Ensure CreateNextTourDate is the name of the procedure.
+        CALL AssignVehicleToTour(current_tour_id);
+    END IF;
+END //
+
+DELIMITER ;
+
+
+
+DELIMITER $$
+
+CREATE TRIGGER reduce_tour_capacity
+AFTER DELETE ON booking
+FOR EACH ROW
+BEGIN
+    DECLARE guests_cancelled INT;
+    DECLARE current_tour_capacity INT;
+    DECLARE tour_id_to_update INT;
+
+    -- Get the number of guests being canceled
+    SET guests_cancelled = OLD.no_of_guests + 1;
+
+    -- Get the current tour's capacity
+    SELECT numofpeople INTO current_tour_capacity FROM tour WHERE tour_id = OLD.tour_id;
+
+    -- Calculate the updated total capacity
+    SET current_tour_capacity = current_tour_capacity - guests_cancelled;
+
+    -- Ensure the minimum capacity is not negative
+    IF current_tour_capacity < 0 THEN
+        SET current_tour_capacity = 0;
+    END IF;
+
+    -- Update the tour with the reduced number of guests
+    UPDATE tour SET numofpeople = current_tour_capacity WHERE tour_id = OLD.tour_id;
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE UpdateTourAndBooking(IN new_booking_id INT)
+BEGIN
+    DECLARE total_guests INT;
+    DECLARE current_tour_capacity INT;
+    DECLARE new_tour_id INT;
+    DECLARE var_start_date DATE;
+    DECLARE var_end_date DATE;
+    DECLARE var_package_id INT;
+    
+    SELECT no_of_guests + 1 INTO total_guests FROM booking WHERE booking_id = new_booking_id;
+    SELECT tour_id, package_id INTO new_tour_id, var_package_id FROM booking WHERE booking_id = new_booking_id;
+    
+    SELECT numofpeople INTO current_tour_capacity FROM tour WHERE tour_id = new_tour_id;
+    SET current_tour_capacity = current_tour_capacity + total_guests;
+
+    IF current_tour_capacity > 20 THEN
+        SELECT start_date, end_date INTO var_start_date, var_end_date FROM tour WHERE tour_id = new_tour_id;
+
+        INSERT INTO tour (numofpeople, start_date, end_date, tour_status, guide_id, package_id)
+        VALUES (total_guests, var_start_date, var_end_date, 'Upcoming', NULL, var_package_id);
+        SET new_tour_id = LAST_INSERT_ID();
+
+        UPDATE booking SET tour_id = new_tour_id WHERE booking_id = new_booking_id;
+    ELSE
+        UPDATE tour SET numofpeople = current_tour_capacity WHERE tour_id = new_tour_id;
+    END IF;
+END$$
+
+DELIMITER ;
+DROP FUNCTION CalculateTourPrice;
+DELIMITER $$
+CREATE FUNCTION CalculateTotalPrice(
+    numGuests INT,
+    basePrice DECIMAL(10, 2)
+) RETURNS DECIMAL(10, 2) deterministic
+BEGIN
+    DECLARE totalPrice DECIMAL(10, 2);
+    DECLARE tax DECIMAL(10, 2);
+
+    -- Calculate total price
+    SET totalPrice = (numGuests + 1) * basePrice; -- Adding 1 for the base price
+    
+    -- Calculate tax
+    SET tax = totalPrice * 0.18; -- 18% tax
+    
+    -- Add tax to the total price
+    SET totalPrice = totalPrice + tax;
+    
+    RETURN totalPrice;
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE AssignVehicleToTour(tourId INT)
+BEGIN
+    DECLARE numberOfPeople INT;
+    DECLARE vehicleTypeToAssign VARCHAR(100);
+
+    -- Get the number of people for the given tour_id
+    SELECT numofpeople INTO numberOfPeople FROM tour WHERE tour_id = tourId;
+
+    -- Check the number of people and determine the vehicle type
+    IF numberOfPeople <= 7 THEN
+        SET vehicleTypeToAssign = 'Mini Van';
+    ELSEIF numberOfPeople BETWEEN 8 AND 12 THEN
+        SET vehicleTypeToAssign = 'Traveller';
+    ELSE
+        SET vehicleTypeToAssign = 'Bus';
+    END IF;
+
+    -- Assign the vehicle to the tour, only if it's unassigned (tour_id is NULL)
+    UPDATE transport_agency
+    SET tour_id = tourId
+    WHERE tour_id IS NULL
+        AND vehicle_type = vehicleTypeToAssign
+    LIMIT 1; -- Limit to assign only one vehicle
+
+END $$
+
+DELIMITER ;
